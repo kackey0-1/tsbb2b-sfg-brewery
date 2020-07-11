c3b1bbd1f0a89d8e53c6c49bbadd23039a167a46
kackey0-1 <kentaro.a.kakimoto@gmail.com>
2020-07-12 08:36:34 +0900

    added test with template

--- src/test/java/guru/springframework/brewery/web/controllers/BeerControllerTestIT.java
+++ src/test/java/guru/springframework/brewery/web/controllers/BeerControllerTestIT.java
@@ -1,11 +1,23 @@
 package guru.springframework.brewery.web.controllers;
 
-import guru.springframework.brewery.domain.Beer;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import guru.springframework.brewery.web.model.BeerPagedList;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@WebMvcTest(BeerController.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class BeerControllerTestIT { //IT=Integration Test
 
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void testListBeers() {
+        BeerPagedList beerPagedList = restTemplate.getForObject("/api/v1/beer", BeerPagedList.class);
+
+        assertThat(beerPagedList.getContent()).hasSize(2);
+    }
 }
