b520fba2a7ee3c60616c6252a27a18b32e6eb887
kackey0-1 <kentaro.a.kakimoto@gmail.com>
2020-07-11 23:00:51 +0900

    added spring boot test

---
+++ src/test/java/guru/springframework/brewery/web/controllers/BeerControllerTestIT.java
@@ -0,0 +1,11 @@
+package guru.springframework.brewery.web.controllers;
+
+import guru.springframework.brewery.domain.Beer;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@WebMvcTest(BeerController.class)
+class BeerControllerTestIT { //IT=Integration Test
+
+}
